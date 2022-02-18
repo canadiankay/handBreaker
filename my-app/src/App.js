97338e0f57e5160bfcbf3d7f6f63caa59b425a28
Nadia <kasimnadia@gmail.com>
2022-02-18 14:22:49 -0500

    set up react for game project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+// deals mostly with the routes
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            {/* <li>
+              <Link to="/game">handBreaker</Link>
+            </li> */}
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register">
+            <h1>REGISTER</h1> 
+          </Route>
+          <Route path="/login">
+            <h1>LOGIN</h1> 
+          </Route>
+          <Route path="/">
+            <h1>HOME</h1> 
+          </Route>
+          {/* <Route path="/game">
+            <h1>handBreaker</h1> 
+          </Route> */}
+
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
