16819d803113afb4c35e07bc8d43e7befe0b86f1
Nadia <kasimnadia@gmail.com>
2022-02-18 16:38:08 -0500

    create paddle and its position relative to canvas

--- my-app/src/games/game/Canvas.js
+++ my-app/src/games/game/Canvas.js
@@ -10,11 +10,32 @@ export default function Canvas() {
   useEffect(() => {
     //We can access the canvas element through the canvasRef now. Now we just need to get the context and start drawing
     const canvas = canvasRef.current //current is a property inside the useRef
-    const context = canvas.getContext('2d') 
+    const ctx = canvas.getContext('2d') 
 
-    //draw a rectangle
-    context.fillStyle = 'white' //this loads a white rectangle on top of canvas
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+    ////////////////////////////// VARIABLES AND CONSTANTS///////////////////
+    // Paddle description
+    const paddle_width = 100;
+    const paddle_height = 20;
+    const paddle_margin_bottom = 50;
+
+    // paddle object + initial position 
+    const paddle = {
+      x: canvas.width / 2 - paddle_width / 2,
+      y: canvas.height - paddle_height - paddle_margin_bottom, //this is constant wont change in the game
+      width: paddle_width,
+      height: paddle_height,
+      dx: 5 //this is amount of pixels paddle will move to the right/left-- wont change
+    }
+
+
+
+    /////////////////////////////////DRAW PADDLE////////////////
+    ctx.fillStyle = 'yellow'
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.strokeStyle = "#ffcd05";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+
   }, [])
     //^ this empty bracket allows it to load initially 
 
