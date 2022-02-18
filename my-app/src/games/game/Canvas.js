563125527291f98dbc91c3070c4a54a0992f1345
Nadia <kasimnadia@gmail.com>
2022-02-18 17:00:37 -0500

    create function loop + animation to allow ball to move

--- my-app/src/games/game/Canvas.js
+++ my-app/src/games/game/Canvas.js
@@ -6,53 +6,62 @@ export default function Canvas() {
 
   const canvasRef = useRef(null) //initiate canvas as null first (from react) then we will utlizie it
 
+  let x = 0;
+
 //it's a component deadmount
   useEffect(() => {
-    //We can access the canvas element through the canvasRef now. Now we just need to get the context and start drawing
-    const canvas = canvasRef.current //current is a property inside the useRef
-    const ctx = canvas.getContext('2d') 
-
-    ////////////////////////////// VARIABLES AND CONSTANTS///////////////////
-    // Paddle description 
-    const paddle_width = 100;
-    const paddle_height = 20;
-    const paddle_margin_bottom = 50;
-
-    // paddle object + initial position 
-    const paddle = {
-      x: canvas.width / 2 - paddle_width / 2,
-      y: canvas.height - paddle_height - paddle_margin_bottom, //this is constant wont change in the game
-      width: paddle_width,
-      height: paddle_height,
-      dx: 5 //this is amount of pixels paddle will move to the right/left-- wont change
-    }
-
-      // Draw ball object + initial position 
-      const ball_radius = 8;
-      const ball = {
-        // the x and y position is at the center of the circle
-        x: canvas.width / 2,
-        y: paddle.y - ball_radius,
-        radius: ball_radius,
-        speed: 4,
-        dx: 3 * (Math.random() * 2 - 1),  // this allows ball to go into random positions-- will generate random numbers between 3 and -
-        dy: -3
+    const loop = () => { //We can access the canvas element through the canvasRef now. Now we just need to get the context and start drawing
+      const canvas = canvasRef.current //current is a property inside the useRef
+      const ctx = canvas.getContext('2d') 
+  
+      ////////////////////////////// VARIABLES AND CONSTANTS///////////////////
+      // Paddle description 
+      const paddle_width = 100;
+      const paddle_height = 20;
+      const paddle_margin_bottom = 50;
+  
+      // paddle object + initial position 
+      const paddle = {
+        x: canvas.width / 2 - paddle_width / 2,
+        y: canvas.height - paddle_height - paddle_margin_bottom, //this is constant wont change in the game
+        width: paddle_width,
+        height: paddle_height,
+        dx: 5 //this is amount of pixels paddle will move to the right/left-- wont change
       }
+  
+        // Draw ball object + initial position 
+        const ball_radius = 8;
+        const ball = {
+          // the x and y position is at the center of the circle
+          x: canvas.width / 2,
+          y: paddle.y - ball_radius,
+          radius: ball_radius,
+          speed: 4,
+          dx: 3 * (Math.random() * 2 - 1),  // this allows ball to go into random positions-- will generate random numbers between 3 and -
+          dy: -3
+        }
+  
+  
+  
+      // /////////////////////////////////DRAW PADDLE////////////////
+      // ctx.fillStyle = 'yellow'
+      // ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+      // ctx.strokeStyle = "#ffcd05";
+      // ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+  
+      ////////////// DRAW BALL //////////////////////////////////
+      ctx.beginPath();
+      // takes in the ball's x and y position, radius and start angle and end angle (which is angle of circle)
+      ctx.arc(x, ball.y, ball.radius, 0, Math.PI * 2);
+      ctx.stroke(); //to draw circle
+      x++;
+     
+      requestAnimationFrame(loop); // this keeps rendering the function and allows ball to move
+  
+    };
+    loop();
 
-
-
-    // /////////////////////////////////DRAW PADDLE////////////////
-    // ctx.fillStyle = 'yellow'
-    // ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
-    // ctx.strokeStyle = "#ffcd05";
-    // ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
-
-    ////////////// DRAW BALL //////////////////////////////////
-    ctx.beginPath();
-    // takes in the ball's x and y position, radius and start angle and end angle (which is angle of circle)
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.stroke(); //to draw circle
-
+  
 
   }, [])
     //^ this empty bracket allows it to load initially 
