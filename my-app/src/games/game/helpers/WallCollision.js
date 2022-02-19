d1d73131216c2b7c3c6e0db4f060484d685290f4
Nadia <kasimnadia@gmail.com>
2022-02-19 02:09:59 -0500

    add logic that reduces lives everytime ball hits bottom wall

--- my-app/src/games/game/helpers/WallCollision.js
+++ my-app/src/games/game/helpers/WallCollision.js
@@ -1,13 +1,26 @@
-const WallCollision = (canvas, ball) => {
+const WallCollision = (canvas, ball, player) => {
   // prevent ball from going beyond our canvas frame
   // top and bottom
-  if (ball.y - ball.radius <= 0 || ball.y + ball.radius >= canvas.height) {
-    ball.dy *= -1; //deflect it in the other directio
+  // if (ball.y - ball.radius <= 0 || ball.y + ball.radius >= canvas.height) {//// the second half of this handles when the ball hits the bottom which we no longer want for score
+  
+  //when ball hits top wall
+  if (ball.y - ball.radius <= 0) {
+    ball.dy *= -1;
   }
-  //side wall
+
+  // when ball hits bottom wall
+  if (ball.y + ball.radius >= canvas.height) {
+    player.lives -= 1
+    //deflect ball
+    ball.dy *= -1
+
+  }
+
+  //handles side walls
   if (ball.x + ball.radius >= canvas.width || ball.x - ball.radius <= 0) {
     ball.dx *= -1
   }
 }
 
-export default WallCollision
+
+export default WallCollision;
