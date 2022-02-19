90bf6edfc97774680e651e7b2cb81a4534dc06d4
Nadia <kasimnadia@gmail.com>
2022-02-19 00:22:16 -0500

    implement bricks and fix bug where paddle moves across entire canvas

--- my-app/src/games/game/Canvas.js
+++ my-app/src/games/game/Canvas.js
@@ -22,13 +22,25 @@ export default function Canvas() {
       const canvas = canvasRef.current //current is a property inside the useRef
       const ctx = canvas.getContext('2d') 
 
+      /////////////// BRICK CREATION ///////////////////////////
       // before clearing canvas we will assign (a) brick(s)
       // we will initiate it with 2 rows
-      CreateBrick(brick, canvas, 2, bricks);
-
+      // CreateBrick(brick, canvas, 2, bricks); // returns an array of objects
+            // ^this calls brick infinite amount of times
+      let allBricks = CreateBrick(brick, canvas, 2, bricks); 
+      // need to handle if all the bricks are undefined or empty 
+      if (allBricks && allBricks.length > 0) {
+        bricks = allBricks; // if they are then we assign it to bricks(empty array)
+      }
 
       ctx.clearRect(0, 0, canvas.width, canvas.height) // clears each new render of the circle so doesnt leave a trail
   
+      //////////////////// DISPLAY BRICKS////////////////////////////
+      // as soon as we have bricks, we want to draw/display each individual brick on the canvas
+      //display bricks
+      bricks.map((brick) => {
+        return brick.draw(ctx);
+      })
   
       // /////////////////////////////////DRAW + MOVE PADDLE////////////////
       // Paddle(ctx, canvas, paddle);
@@ -62,9 +74,14 @@ export default function Canvas() {
         id="myCanvas" 
         ref= {canvasRef} 
         height="600" 
-        // width="1000"
-        width={ window.innerWidth - 25 }
-        onMouseMove={(event) => (paddle.x = event.clientX - paddle.width / 2)} //allows paddle to move via mouse
+        width="1000"
+        // width={ window.innerWidth - 25 }
+        //allows paddle to move via mouse
+        //onMouseMove={(event) => {paddle.x = event.clientX - paddle.width / 2; console.log(paddle.x)}} // this works with width being inner width
+        onMouseMove={(event) => {
+          paddle.x = event.clientX - 500; 
+          // console.log(paddle.x)
+        }} // this works with setting width to 1000
         
         /> 
         
