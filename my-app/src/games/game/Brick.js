d6113591d647722972531981b840ca3ac182f154
Nadia <kasimnadia@gmail.com>
2022-02-19 00:23:50 -0500

    change color of bricks

--- my-app/src/games/game/Brick.js
+++ my-app/src/games/game/Brick.js
@@ -51,7 +51,7 @@ class Brick {
   draw(ctx) {
     ctx.beginPath();
     ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.fillStyle = this.broke ? "rgba(19, 73, 89, 0)" : this.colors[1];
+    ctx.fillStyle = this.broke ? "rgba(19, 73, 89, 0)" : this.colors[0];//this is in reference to the array of colors in data
 
     ctx.lineWidth = 5;
     ctx.strokeStyle = this.broke ? "rgba(19, 73, 89, 0)" : "transparent";
