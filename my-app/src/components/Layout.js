5dbcd7c2b44163c46425e5d1ceea78fbd1130ce2
Nadia <kasimnadia@gmail.com>
2022-02-20 18:02:15 -0500

    update layout to include routers to appropriate sections

--- my-app/src/components/Layout.js
+++ my-app/src/components/Layout.js
@@ -1,19 +1,37 @@
 // this is the whole page-- for styling
 import styles from '../styles/layout.css'
 
+//use router
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 // import nav bar and header
-import Nav from './nav'
-import Header from './header'
-import Footer from './footer'
+import Nav from './Nav.js'
+import Header from './Header.js'
+import Footer from './Footer'
+import About from './About'
 
 
 const Layout = () => {
   return (
+    <Router>
     <div className={styles.container}>
-      <Nav />
-      <Header />
+      <Route 
+        path="/" 
+        exact 
+        render={(props)=> (
+        <>
+          <Nav />
+          <Header />
+        </>
+        )} 
+      /> 
+      <Route path="/about" component={About} />
       <Footer />
-    <div/>
+      
+    </div>
+    </Router>
+    
+    
       
     
   )
