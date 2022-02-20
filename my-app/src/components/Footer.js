26093b00f89f2ad9d20988d65eee1daf54b472ee
Nadia <kasimnadia@gmail.com>
2022-02-20 17:31:35 -0500

    create footer component

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+
+      <p>Copyright DMN Games &copy; 2022</p>
+
+      <a href="/about">ABOUT US</a>
+
+
+    </footer>
+  )
+}
+
+export default Footer
