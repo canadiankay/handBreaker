ae7059787cc0c0bde6b66c10d514f1ee59d72545
Nadia <kasimnadia@gmail.com>
2022-02-20 21:07:48 -0500

    update styling for footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 const Footer = () => {
+  const footerStyle = {
+    padding: '10px',
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+    color: 'white',
+  }
   return (
-    <footer>
+    <footer style={footerStyle}>
       
       <h4>DMN Games Version 1.0.0 </h4>
       
       <p>Copyright DMN Games &copy; 2022</p>
-      <a href="/about">ABOUT US</a>
-
 
     </footer>
   )
