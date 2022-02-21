4d0ce07ec1b423064f2ce0934bf641de91967b3a
Nadia <kasimnadia@gmail.com>
2022-02-21 14:26:24 -0500

    add timed pop up

--- my-app/src/games/game/index.js
+++ my-app/src/games/game/index.js
@@ -2,7 +2,7 @@
 import { useHistory } from "react-router-dom";
 //the main game component-- handbreaker
 // entry point into the game----- index.js
-import {useState}  from 'react'
+import {useState, useEffect}  from 'react'
 import Canvas from './Canvas.js'
 import Popup from 'reactjs-popup';
 import Button from '../../components/Button.js'
@@ -12,6 +12,10 @@ import Popup1 from '../../components/Popup1.js'
 export default function Game () {
   // this will set that variable for the pop up to true 
   const [gamePopup, setGamePopup] = useState(false);
+  const [timedPopup, setTimedPopup] = useState();
+  setTimeout(() => {
+    setTimedPopup(true);
+  }, 3000)
 
   const history = useHistory();
   const goToLeaderboard = () => history.push('/leaderboard');//eg.history.push('/login');
@@ -68,6 +72,12 @@ export default function Game () {
         <Popup1 trigger={gamePopup} setTrigger={setGamePopup}>
             <h2>my Pop up</h2>
             <p>Welcome to handBreaker</p>
+        </Popup1>
+
+          {/* this is our timed popup */}
+        <Popup1 trigger={timedPopup} setTrigger={setTimedPopup}>
+            <h2>my Timed Pop up</h2>
+            <p>My time triggered pop up</p>
         </Popup1>
 
 
