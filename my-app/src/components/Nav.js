58fced1e2b29fa1480280b7d55698b29985aca73
Nadia <kasimnadia@gmail.com>
2022-02-20 20:23:55 -0500

    update nav bar to include logo

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,32 +1,33 @@
-import navStyles from '../styles/Nav.css'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import logo from "../imgs/DMN-Games-Logo.png"
 function Nav() {
   return (
-    <nav className={navStyles.nav}>
+    <nav>
+      {/* insert logo */}
+      <img src={logo} />
+
       <a href="/">HOME</a>
 
       <a href="/about">ABOUT US</a>
 
-      <ul>
-          <Popup  
-            trigger={<button text='Register'>REGISTER</button>} 
-            position="bottom center">
-              <h4>Username:<input placeholder='Username'></input></h4>
-              <h4>Email:<input placeholder='example@gmail.com'></input></h4>
-              <h4>Password:<input placeholder='Password'></input></h4>
-              <h4>Confirm Password:<input placeholder='Confirm password'></input></h4>
-              <input type="Submit" value="Submit"></input>
-          </Popup>
+      <Popup  
+        trigger={<button text='Register'>REGISTER</button>} 
+        position="bottom center">
+          <h4>Username:<input placeholder='Username'></input></h4>
+          <h4>Email:<input placeholder='example@gmail.com'></input></h4>
+          <h4>Password:<input placeholder='Password'></input></h4>
+          <h4>Confirm Password:<input placeholder='Confirm password'></input></h4>
+          <input type="Submit" value="Submit"></input>
+      </Popup>
 
-          <Popup trigger={<button text='Login'>LOGIN</button>} 
-            position="bottom center">
-              <h4>Username:<input placeholder='Username'></input></h4>
-              <h4>Password:<input placeholder='Password'></input></h4>
-              <input type="Submit" value="Submit"></input>
-          </Popup>
-    
-        </ul>
+      <Popup trigger={<button text='Login'>LOGIN</button>} 
+        position="bottom center">
+          <h4>Username:<input placeholder='Username'></input></h4>
+          <h4>Password:<input placeholder='Password'></input></h4>
+          <input type="Submit" value="Submit"></input>
+      </Popup>
+
     </nav>
   )
 }
