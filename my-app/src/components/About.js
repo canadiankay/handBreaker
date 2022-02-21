78f8ca3af9ecba6567e5a1f1228ce39962ee1a46
Nadia <kasimnadia@gmail.com>
2022-02-20 21:05:51 -0500

    add elements + styling as per our sketch

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,11 +1,50 @@
 
 
 const About = () => {
+
+  const aboutStyle = {
+    color: "orange",
+    backgroundColor: "grey",
+    fontFamily: "Helvetica",
+    textAlign: "center",
+    fontSize: "50px",
+  }
+
+  const eachDeveloperStyle = {
+    width: "50%",
+    height: "50px",
+    color: "red",
+    backgroundColor: "orange",
+    fontFamily: "Helvetica",
+    textAlign: "center",
+    fontSize: "25px",
+    margin: "30px 500px"
+  }
   return (
     <div>
-      <h4>Version 1.0.0 </h4>
+      <main style={aboutStyle}>
+        <p> 
+        A group of entry-level devleoppers, Dev, Nadia and Michel, interested in the world of AI and Gaming.
+        We want to create the next gaming portal for AI games only
+        </p>
+      </main>
 
-      <a href="/">GO BACK TO HOME</a>
+      <body> 
+
+        <section style={eachDeveloperStyle}>
+          <a href="https://github.com/devendrachitanna">Devendra Chitanna</a>
+        </section>
+
+      <section style={eachDeveloperStyle}>
+          <a href="https://github.com/MotongiM">Michel Motongi</a>
+        </section>
+
+        <section style={eachDeveloperStyle}>
+          <a href="https://github.com/canadiankay">Nadia Kasim</a>
+        </section>
+
+      </body>
+    
     </div>
   )
 }
