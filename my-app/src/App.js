018f5659a2ea9c80153d95fcbf53ca5602a11f42
Nadia <kasimnadia@gmail.com>
2022-02-20 23:27:05 -0500

    commented out reacr router stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 // deals mostly with the routes
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 // import Canvas from './games/game/Canvas';
 // import TheGame from './games/game/index.js';
 // import Game from './games/game/index.js';
