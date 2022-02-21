1a4b68faeed5dc7519733ab21103bf9622e1d8a3
Nadia <kasimnadia@gmail.com>
2022-02-21 04:30:18 -0500

    create individual leader component

---
+++ my-app/src/components/Leader.js
@@ -0,0 +1,13 @@
+// this will be each individual leader component
+
+import React from 'react'
+
+const Leader = () => {
+  return (
+    <div>
+      We want each leader here
+    </div>
+  )
+}
+
+export default Leader
