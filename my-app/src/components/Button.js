c88261b42aec0eae08872f4bf559753e76cc3d43
Nadia <kasimnadia@gmail.com>
2022-02-20 23:44:20 -0500

    git create button component

---
+++ my-app/src/components/Button.js
@@ -0,0 +1,15 @@
+// default for any button
+
+
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
