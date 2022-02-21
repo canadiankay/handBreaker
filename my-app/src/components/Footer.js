e0f3a836c7787265b1e1f462ca7b6f6e9594a3b7
Nadia <kasimnadia@gmail.com>
2022-02-20 20:24:32 -0500

    update footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-
+      
+      <h4>DMN Games Version 1.0.0 </h4>
+      
       <p>Copyright DMN Games &copy; 2022</p>
       <a href="/about">ABOUT US</a>
 
