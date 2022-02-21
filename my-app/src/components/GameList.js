f939f21842901d30cfc0321373715db9fdc3a872
Nadia <kasimnadia@gmail.com>
2022-02-20 23:26:08 -0500

    add gamelist component where each game will display on Home Page

---
+++ my-app/src/components/GameList.js
@@ -0,0 +1,15 @@
+//this will go into the main page body where it will show all of the games
+
+import React from 'react'
+
+const GameList = () => {
+  return (
+    <div>
+      <body style={{height:"200px"}}>
+        <h2>Go to <a href="/game"> THE GAME</a></h2>
+      </body>
+    </div>
+  )
+}
+
+export default GameList
