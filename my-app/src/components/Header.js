14e60c31bc3051610b4428196041f82a5c814406
Nadia <kasimnadia@gmail.com>
2022-02-24 01:38:18 -0500

    add shape to header and integrate ai image and site title

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,21 +1,15 @@
 
 import background from "../imgs/Ai.jpeg"
+import "./header.css"
 
 const Header = () => {
-  const headerStyles = {
-    backgroundImage:`url(${background})`,
-    fontSize:"50px",
-    color:"white",
-    height:"500px",
-    display:"flex", 
-    alignItems:"center",
-    justifyContent:"center"
-
-  }
+  
 
   return (
-      <header style={headerStyles}>
-       <div style={{background:"black"}}>Welcome to DMN Games 🕹 🎮✨</div>
+      <header>
+       {/* <div style={{background:"black"}}>Welcome to MDN Games 🕹 🎮✨</div> */}
+       <div className="header-title" >WELCOME TO MDN GAMES</div>
+       <div className="div-ai"><img src={background} className="img-ai"/></div>
       </header>
     
   )
