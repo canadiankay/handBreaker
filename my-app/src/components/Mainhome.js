a64a873a67a9a10d4f3fa363db2807415904ff6a
Nadia <kasimnadia@gmail.com>
2022-02-24 05:15:59 -0500

    create container to go on the bottom of home page with game

---
+++ my-app/src/components/Mainhome.js
@@ -0,0 +1,23 @@
+// bottom container where game will go middle of home page
+import Game from '../games/game/index.js'
+
+import "./home.css"
+
+const Mainhome = ({userInfo}) => {
+  return (
+    <div className="game-container">
+      <h2>HANDBREAKER</h2>
+     
+
+
+
+     
+     
+
+
+      
+    </div>
+  )
+}
+
+export default Mainhome
