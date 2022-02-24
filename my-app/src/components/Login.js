1652cc18cc4ec5c4ac908b1f6c566a9e05db05bc
Nadia <kasimnadia@gmail.com>
2022-02-24 03:31:07 -0500

    style login page

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
+import "./login.css"
+
 const Login = ({ auth, toggleLoggedIn, setUserInfo }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -37,30 +39,49 @@ const Login = ({ auth, toggleLoggedIn, setUserInfo }) => {
   };
 
   return (
-    <form>
-      <span className="login-error">{error}</span>
-      <div>
-        Email:
-        <input
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </div>
-      <div>
-        Password:
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </div>
-      <button onClick={loginHandler} type="button">
-        Login
-      </button>
-    </form>
+    <div className="login-page">
+      <div class="login-box">
+      <h2>Login</h2>
+        <form>
+          
+          <span className="login-error">{error}</span>
+          <div className="user-box">
+            <input
+              type="text"
+              // placeholder="Please enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              name=""
+              required=""
+            />
+            <label>Email:</label>
+          </div>
+
+          <div className="user-box">
+            <input
+              type="password"
+              // placeholder="Please enter a password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              name=""
+              required=""
+            />
+            <label>Password:</label>
+          </div>
+
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <button onClick={loginHandler} type="button" >SUBMIT</button>
+          </a>
+
+
+          
+        </form>
+    </div>
+    </div>
   );
 };
 
