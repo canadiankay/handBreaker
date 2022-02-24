87a11169df01c7da15e918895ab89e5fb36c7241
Nadia <kasimnadia@gmail.com>
2022-02-23 22:16:47 -0500

    add styling to NavBar

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,42 +1,39 @@
 // import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import logo from "../imgs/DMN-Games-Logo.png"
+import logo from "../imgs/MDN-Logo.png"
 import { Link} from 'react-router-dom'
-// import {useState} from 'react'
+import "./nav.css"
 
 function Nav({isLoggedIn}) {
 
-  const navStyles={
-    background:"orange",
-    display:"flex",
-    alignItems: "center",
-    justifyContent:"space-evenly",
-    color: "white",
-    width: "100%",
-    height: "100px"
+  const linkStyles={
+    fontFamily: "'Exo', sans-serif",
+    fontSize:"20px",
+    color:"white"
+
   }
 
   return (
-    <nav style={navStyles}>
+    <nav>
       {/* insert logo */}
-      <img src={logo} alt="" width="90px" height="90px" border-radius="50%" />
+      <img src={logo} alt="" width="220px" height="220px" border-radius="50%" />
 
   
-      <Link to="/">HOME</Link>
+      <Link to="/" style={linkStyles}>HOME</Link>
 
-      <Link to="/about">ABOUT</Link>
+      <Link to="/about" style={linkStyles}>ABOUT</Link>
 
 
       {/* if user is logged in  */}
       {isLoggedIn &&
-        <Link to="/game" onClick={() => window.location.reload(false)}>LOGOUT</Link>
+        <Link to="/" onClick={() => window.location.reload(false)} style={linkStyles}>LOGOUT</Link>
       }
 
       {/* if user is not logged in */}
       {!isLoggedIn && (
         <>
-          <Link to="/register">REGISTER</Link>
-          <Link to="/login">LOGIN</Link>
+          <Link to="/register" style={linkStyles}>REGISTER</Link>
+          <Link to="/login" style={linkStyles} className="boxed">LOGIN</Link>
         </>)
       }
 
